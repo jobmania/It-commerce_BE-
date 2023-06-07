7835a4bf148c3cbda61929453425548cbbf8166d
Son Won Cheol <xzxz7003@naver.com>
2023-06-07 12:18:13 +0900

    Spring Security 권한설정

--- src/main/java/com/sideproject/shop/member/service/CustomUserDetailsService.java
+++ src/main/java/com/sideproject/shop/member/service/CustomUserDetailsService.java
@@ -19,33 +19,33 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
 
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자는 없습니다 이메일 : " + email));
+//     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//         Member member = memberRepository.findByEmail(email)
+//                 .orElseThrow(() -> new UsernameNotFoundException("해당 사용자는 없습니다 이메일 : " + email));
 
-        return new MemberDetailsImpl(member);
-    }
+//         return new MemberDetailsImpl(member);
+//     }
 
 
-//
-//    @Override
-//    @Transactional  // UserDetails 와 Authentication 의 패스워드를 비교하고 검증하는 로직
-//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//        return memberRepository.findByEmail(email)
-//                .map(this::createMember)
-//                .orElseThrow(() -> new UsernameNotFoundException(email + "데이터 베이스에 없음"));
-//    }
-//
-//    // DB 에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
-//    private UserDetails createMember(Member member) {
-//        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
-//
-//        return new User(
-//                String.valueOf(member.getId()),
-//                member.getPassword(),
-//                Collections.singleton(grantedAuthority)
-//        );
-//    }
+ // 로그인 시 권한설정
+   @Override
+   @Transactional  // UserDetails 와 Authentication 의 패스워드를 비교하고 검증하는 로직
+   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       return memberRepository.findByEmail(email)
+               .map(this::createMember)
+               .orElseThrow(() -> new UsernameNotFoundException(email + "데이터 베이스에 없음"));
+   }
+
+   // DB 에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
+   private UserDetails createMember(Member member) {
+       GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
+
+       return new User(
+               String.valueOf(member.getId()),
+               member.getPassword(),
+               Collections.singleton(grantedAuthority)
+       );
+   }
 
 
 }
